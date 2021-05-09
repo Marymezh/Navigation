57906a647763e6f053364b7aed5260c96e9daea2
Marymezh <maria.mezhova@yahoo.com>
2021-05-09 18:10:26 +0700

    AlertAction was added to the InfoVIewController and the title was changed through prepare and segue function

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -8,22 +8,28 @@
 import UIKit
 
 class FeedViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    struct Post {
+        var title: String
     }
-    */
 
+    var newPost = Post(title: "Новый пост")
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "newPost" else {
+            return
+        }
+        guard let vc = segue.destination as? PostViewController else {
+            return
+        }
+        vc.title = "Новый преновый пост"
+    }
 }
+
+
+
+
+
